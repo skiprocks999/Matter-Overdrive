2f00778d96790f827a02222b3a7cecfb8a745281
Lanse <simon_stalnabb@hotmail.com>
2022-07-29 21:18:12 +0200

    Basic fixes and clean-up of gradle

--- src/main/java/matteroverdrive/core/datagen/client/OverdriveBlockStateProvider.java
+++ src/main/java/matteroverdrive/core/datagen/client/OverdriveBlockStateProvider.java
@@ -3,11 +3,14 @@ package matteroverdrive.core.datagen.client;
 import matteroverdrive.DeferredRegisters;
 import matteroverdrive.References;
 import matteroverdrive.common.block.type.BlockColors;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.Block;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
 import net.minecraftforge.client.model.generators.ModelFile.ExistingModelFile;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.ForgeRegistries;
 
 public class OverdriveBlockStateProvider extends BlockStateProvider {
 
@@ -40,10 +43,16 @@ public class OverdriveBlockStateProvider extends BlockStateProvider {
 		}
 		simpleBlock(DeferredRegisters.BLOCK_SOLAR_PANEL.get());
 		simpleBlock(DeferredRegisters.BLOCK_CHARGER_CHILD.get());
-
-		simpleBlock(DeferredRegisters.BLOCK_INDUSTRIAL_GLASS.get());
+		simpleBlockWithRenderType(DeferredRegisters.BLOCK_INDUSTRIAL_GLASS.get(), RenderType.cutout());
 		simpleBlock(DeferredRegisters.BLOCK_VENT_OPEN.get());
 		simpleBlock(DeferredRegisters.BLOCK_VENT_CLOSED.get());
 	}
 
+	private void simpleBlockWithRenderType(Block block, RenderType renderType) {
+		simpleBlock(block, models().cubeAll(name(block), blockTexture(block)).renderType(renderType.toString()));
+	}
+
+	private String name(Block block) {
+		return ForgeRegistries.BLOCKS.getKey(block).getPath();
+	}
 }
