4bbe01acdacc9f055f5bccdefcc80b866da844dc
skip999 <skiprocks999@gmail.com>
2022-10-02 14:01:41 -0500

    mixin work

--- src/main/java/matteroverdrive/client/render/item/VariableAlphaItemRenderer.java
+++ src/main/java/matteroverdrive/client/render/item/VariableAlphaItemRenderer.java
@@ -14,31 +14,33 @@ import net.minecraft.world.item.ItemStack;
 public class VariableAlphaItemRenderer extends ItemRenderer {
 
 	private float alpha = 1.0F;
-	
+
 	public VariableAlphaItemRenderer(ItemRenderer renderer) {
 		super(renderer.textureManager, renderer.itemModelShaper.getModelManager(), renderer.itemColors,
 				renderer.blockEntityRenderer);
 	}
-	
+
 	public void setAlpha(float alpha) {
 		this.alpha = Mth.clamp(alpha, 0.0F, 1.0F);
 	}
-	
-	public void renderQuadList(PoseStack matrix, VertexConsumer consumer, List<BakedQuad> quads, ItemStack stack, int light, int overlay) {
-	      boolean itemNotEmpty = !stack.isEmpty();
-	      PoseStack.Pose pose = matrix.last();
 
-	      for(BakedQuad bakedquad : quads) {
-	         int color = -1;
-	         if (itemNotEmpty && bakedquad.isTinted()) {
-	            color = this.itemColors.getColor(stack, bakedquad.getTintIndex());
-	         }
+	@Override
+	public void renderQuadList(PoseStack matrix, VertexConsumer consumer, List<BakedQuad> quads, ItemStack stack,
+			int light, int overlay) {
+		boolean itemNotEmpty = !stack.isEmpty();
+		PoseStack.Pose pose = matrix.last();
 
-	         float[] colorArr = UtilsRendering.getColorArray(color);
+		for (BakedQuad bakedquad : quads) {
+			int color = -1;
+			if (itemNotEmpty && bakedquad.isTinted()) {
+				color = this.itemColors.getColor(stack, bakedquad.getTintIndex());
+			}
 
-	         consumer.putBulkData(pose, bakedquad, colorArr[0], colorArr[1], colorArr[2], alpha, light, overlay, true);
-	      }
+			float[] colorArr = UtilsRendering.getColorArray(color);
 
-	   }
+			consumer.putBulkData(pose, bakedquad, colorArr[0], colorArr[1], colorArr[2], alpha, light, overlay, true);
+		}
+
+	}
 
 }
